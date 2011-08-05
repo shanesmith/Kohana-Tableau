02c19a4a4151951187fb080421abf77319d62376
Shane Smith <cap.webmanager@gmail.com>
2011-08-05 18:00:36 -0400

    Added TD->getRowData() shortcut method

--- classes/kohana/tableau/html/td.php
+++ classes/kohana/tableau/html/td.php
@@ -18,5 +18,15 @@ class Kohana_Tableau_HTML_Td extends Tableau_HTML_Cell {
 	public function getView() {
 		return View::factory('tableau/td', array( 'td' => $this ));
 	}
+	
+	/**
+	 * Get data found on the same row as this TD
+	 *
+	 * @param string $column
+	 * @return string|array
+	 */
+	public function getRowData($column=null) {
+		return $this->table->getData($this->row, $column);
+	}
 
 }
